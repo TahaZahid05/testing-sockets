19ad93d22b6d7da88388490033d46b66057bdeac
TahaZahid05 <tz09220@st.habib.edu.pk>
2025-03-23 02:27:01 +0500

    Trying webSockets

---
+++ chatapp.pro
@@ -0,0 +1,19 @@
+QT       += core gui websockets
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = chatapp
+TEMPLATE = app
+
+SOURCES += main.cpp \
+           chatwindow.cpp
+
+HEADERS  += chatwindow.h
+
+FORMS    += chatwindow.ui
+
+# Ensure proper C++ standard
+CONFIG += c++17
+
+# Include Qt WebSockets module
+QT += websockets
---
+++ chatwindow.cpp
@@ -0,0 +1,45 @@
+#include "chatwindow.h"
+#include "ui_chatwindow.h"
+#include <QDebug>
+
+ChatWindow::ChatWindow(QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::ChatWindow) {
+    ui->setupUi(this);
+
+    // Connect WebSocket signals
+    connect(&webSocket, &QWebSocket::connected, this, &ChatWindow::onConnected);
+    connect(&webSocket, &QWebSocket::textMessageReceived, this, &ChatWindow::onMessageReceived);
+    connect(&webSocket, &QWebSocket::disconnected, this, &ChatWindow::onDisconnected);
+
+    // Connect textChanged signal for real-time sending
+    connect(ui->messageInput, &QLineEdit::textChanged, this, &ChatWindow::onTextChanged);
+
+    qDebug() << "Connecting to WebSocket server...";
+    webSocket.open(QUrl("ws://localhost:12345"));  // Connect to Python WebSocket server
+}
+
+ChatWindow::~ChatWindow() {
+    delete ui;
+}
+
+void ChatWindow::onConnected() {
+    qDebug() << "Connected to WebSocket server!";
+}
+
+void ChatWindow::onMessageReceived(QString message) {
+    qDebug() << "Message from server:" << message;
+    ui->chatDisplay->append("Server: " + message);
+}
+
+void ChatWindow::onDisconnected() {
+    qDebug() << "Disconnected from server.";
+}
+
+// New function to send messages in real-time
+void ChatWindow::onTextChanged(const QString &text) {
+    if (webSocket.isValid() && !text.isEmpty()) {
+        webSocket.sendTextMessage(text);
+        qDebug() << "Sent:" << text;
+    }
+}
